808153da6d5f401930ad214e48b714f52ec38ebf
Ryan Engle <rtengle2001@gmail.com>
2025-02-08 20:54:09 -0500

    Added newton method

--- README.md
+++ README.md
@@ -12,32 +12,36 @@ This repository contains code for Assignment 1 in ME 700, including functions fo
 
 # Newton's Method Algorithm
 
-1. Start with a fully differentiable function $\vec{f}(\vec{x})$ and an initial guess $\vec{x}_0.$
+1. Start with a fully differentiable function $\vec{f}(\vec{x}): \mathbb{R}^n\rightarrow \mathbb{R}^m$ and an initial guess $\vec{x}_0 \in \mathbb{R}^n.$
 2. Compute the Jacobian $\mathbf{J}(\vec{f}, \vec{x}_n), $ of the matrix as defined by:
 ```math 
 \mathbf{J}\left(\vec{f}, \vec{x}_n\right) = \left[
     \begin{matrix}
         \frac{\partial f_1}{\partial x_1} & \cdots & \frac{\partial f_1}{\partial x_n} \\
         \vdots  & \ddots & \vdots \\
-        \frac{\partial f_n}{\partial x_1} & \cdots & \frac{\partial f_n}{\partial x_n}
+        \frac{\partial f_m}{\partial x_1} & \cdots & \frac{\partial f_m}{\partial x_n}
     \end{matrix} 
  \right] \Big |_{\vec{x} = \vec{x}_n}
 ```
 3. Compute the next vector as:
 ```math
-\vec{x}_{n+1} = \vec{x}_n - \mathbf{J}\left(\vec{f}, \vec{x}_n \right) \vec{f}(\vec{x}_n)
+\vec{x}_{n+1} = \vec{x}_n - \mathbf{J}^{\mathbb{I}}\left(\vec{f}, \vec{x}_n \right) \vec{f}(\vec{x}_n)
 ```
+where $\mathbf{A}^\mathbb{I}$ is the relevant generalized inverse function for the matrix $\mathbb{A} \in \mathbb{R}^{n \times m}$:
+  - If $n = m$, then a standard inverse is used.
+  - If $n > m$, then the right-handed inverse is used defined by $\mathbf{A}^\mathbb{I} = \mathbf{A}^T \left( \mathbf{A} \mathbf{A}^T\right)^{-1}$
+  - If $n < m$, then the left-handed inverse is used defined by $\mathbf{A}^\mathbb{I} = \left(\mathbf{A}^T \mathbf{A} \right)^{-1} \mathbf{A}$
 4. Iterate until either convergence is reached or the step limit is reached.
 # Installation
 
 To install this package, please begin by setting up a conda environment (mamba also works):
 ```bash
-conda create --name bisection-method-env python=3.9.18
+conda create --name me700-hw1-env python=3.12
 ```
 Once the environment has been created, activate it:
 
 ```bash
-conda activate bisection-method-env
+conda activate me700-hw1-env
 ```
 Double check that python is version 3.9.18 in the environment. It should still work on a later version, but it was made on this one.
 ```bash
@@ -53,6 +57,6 @@ pip install -e .
 ```
 Test that the code is working with pytest:
 ```bash
-pytest -v --cov=. --cov-report=
+pytest -v --cov=. --cov-report=xml
 ```
 Code coverage should be 100%. Now you are prepared to write your own code based on this method and/or run the tutorial.
---
+++ src/newton.py
@@ -0,0 +1,134 @@
+import numpy as np
+from numpy import linalg as la
+from typing import Callable
+
+# Defines an example 
+def example_function(x: np.ndarray):
+    return x ** 2
+
+def jacobian(f: Callable[[np.ndarray], np.ndarray], x: np.ndarray, eps:float=2.22e-16) -> np.ndarray:
+    """Computes the Jacobian matrix for a function f(x) based on an input x using the center difference method.
+
+    Args:
+        f: Input function f(x). Must accept and return a numpy array.
+        x: Input variable x. Must be a numpy array or scalar.
+        eps: Machine epsilon of data type in ndarray used to determine distance used. Default is 2.22e-16 corresponding to float64.
+    """
+    # Converts scalar x into ndarray x
+    x = floatarray_convert(x)
+
+    # Sets up difference vector dx and the number of dimenions in x
+    dx = np.array(x*np.sqrt(eps))
+    Nx = len(x)
+
+    # Determines dimension of output
+    f0 = floatarray_convert(f(x))
+    Nf = len(f0)
+
+    # Initializes Jacobian matrix J
+    J = np.zeros([Nf, Nx])
+
+    # Iterates through x-variables, determining the Jacobian column-by-column
+    for i in range(Nx):
+        # Isolates difference direction dx to the component x_i
+        # Checks to see if x variable is smaller than esp to avoid numerical errors
+        dxn = np.zeros(Nx)
+        if dx[i] < eps:
+            dxn[i] = eps
+        else:
+            dxn[i] = dx[i]
+
+        # Calculates the partial derivative of x_i and assigns the output to the Jacobian
+        J[:, i] = center_difference(f, x, dxn).T
+
+    return J
+
+def center_difference(f: Callable[[np.ndarray], np.ndarray], x: np.ndarray, dx: np.ndarray) -> np.float64:
+    """Performs center difference across a function f(x) in the direction dx.
+
+    Args:
+        f: Input function f(x). Must accept and return one numpy array.
+        x: Location of the center difference. Must be a numpy array.
+        dx: Direction array along which the difference takes place. Must be a numpy array.
+
+    """
+    # Left and right function values
+    a = f(x+dx)
+    b = f(x-dx)
+    # Returns the center difference result
+    return (a-b)/(2*la.norm(dx))
+
+def floatarray_convert(x):
+    # Converts any non-ndarray to ndarray
+    if type(x) == np.ndarray:
+        return x
+    else:
+        return np.array([x])
+    
+def floatarray_extract(x):
+    # Returns value of single-element ndarray
+    if np.shape(x) == (1) or np.shape(x) == (1,):
+        return x[0]
+    else:
+        return x
+
+def newton(f: Callable[[np.ndarray], np.ndarray], x0: np.ndarray, tol:float = 1e-6, maxiter:int = 50):
+    """Finds the value value y = f(x) where norm(y) = 0. 
+
+    Args:
+        f: Input function f(x). Input and output must be 1D vectors as ndarray.
+        x0: Initial guess for the method.
+        tol: Convergence tolerance for bisection method. Default 1e-6, must be positive.
+        N (int): Maximum number of loops. Default 50, must be positive.
+    
+    Returns:
+        x: List of guesses
+        y: List of values for f(x)
+    """
+
+    # Initializes the list of guesses and values. Floatarray_convert is used to handle if the input or output is a scalar
+    x = [floatarray_convert(x0)]
+    y = [floatarray_convert(f(x0))]
+
+    # Checks tolerance and maxiter for proper ranges
+    if tol<0:
+        raise Exception('Tolerance must be positive')
+    if maxiter <= 0:
+        raise Exception('Maximum number of iterations must be positive')
+    
+    # Checks to make sure input and output are 1D
+    if len(x[0].shape) > 1:
+        raise Exception('Input must be 1D numpy array or scalar')
+    if len(y[0].shape) > 1:
+        raise Exception('Function output must be 1D numpy array or scalar')
+    
+    # Gets the number of input and output dimensions
+    Nx = len(x[0])
+    Nf = len(y[0])
+    
+    # Checks if input guess is good enough as-is
+    if la.norm(y[0]) <= tol:
+        return x, y
+    
+    # Main iterative loop
+    for i in range(maxiter):
+        # Calculates the Jacobian of the function at the input
+        J = jacobian(f, x[i])
+        # Performs the relevant generalized inverse as described in the README then appends the change to the guess list
+        if Nx == Nf:
+            # Square inverse
+            x.append(x[i] - la.inv(J) @ y[i])
+        elif Nx > Nf:
+            # Right inverse
+            x.append(x[i] - J.T @ la.inv(J @ J.T) @ y[i])
+        else:
+            # Left inverse
+            x.append(x[i] - la.inv(J.T @ J) @ J.T @ y[i])
+        # Appends the result of the guess to the results list
+        y.append(floatarray_convert(f(floatarray_extract(x[i+1]))))
+        
+        # Returns the lists if the tolerance is reached
+        if la.norm(y[i+1]) <= tol:
+            return x, y
+
+    return x, y
